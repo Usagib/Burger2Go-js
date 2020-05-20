83c03e40b5ba7ace217335db17f306040a493f18
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-05-20 14:07:43 -0500

    Tabs now working

--- src/index.js
+++ src/index.js
@@ -6,104 +6,131 @@ const init = () => {
   document.getElementById('content').setAttribute("class", "container-fluid");
 }
 
-const homeBanner = () => {
+const menuTab = () => {
+
+}
+
+const homeTab = () => {
+
+  function attributesSet() {
+    banner.setAttribute("class", "container");
+    row.setAttribute("class", "row banner-row");
+    featurerow.setAttribute("class", "row");
+    feat1.setAttribute("class", "col-sm-4 feature-div-1");
+    feat2.setAttribute("class", "col-sm-4 feature-div-2");
+    feat3.setAttribute("class", "col-sm-4 feature-div-3");
+  }
+
+  function render() {
+    banner.appendChild(row);
+    featurerow.appendChild(feat1);
+    featurerow.appendChild(feat2);
+    featurerow.appendChild(feat3);
+    banner.appendChild(featurerow);
+  }
+
   let banner = document.createElement("div");
-  banner.setAttribute("class", "container-fluid");
-  let row = document.createElement("div");
-  row.setAttribute("class", "row banner-row");
-  document.getElementById('content').appendChild(banner);
-  banner.appendChild(row);
-}
-
-const navigationBar = () => {
-  let nav = document.createElement("nav");
-  nav.setAttribute("class", "navbar navbar-expand-lg navbar-light bg-light");
-  let container = document.createElement("div");
-  container.setAttribute("class", "container");
-  let logo = document.createElement("a");
-  logo.setAttribute("class", "navbar-brand brand-logo");
-  let logoImg = new Image();
-  logoImg.setAttribute("class", "brand-img");
-  let toggler = document.createElement("button");
-  toggler.setAttribute("class", "navbar-toggler");
-  let togglerIcon = document.createElement("span");
-  togglerIcon.setAttribute("class", "navbar-toggler-icon");
-  let collapseNavbar = document.createElement("div");
-  collapseNavbar.setAttribute("id", "navbarSupportedContent");
-  let collapseUl = document.createElement("ul");
-  collapseUl.setAttribute("class", "navbar-nav ml-auto");
-  let collapseLi = document.createElement("li");
-  collapseLi.setAttribute("class", "nav-item dropdown");
-  let dropdownToggle = document.createElement("a");
-
-  let dropdownMenu = document.createElement("div");
-
-  let dropdownItem = document.createElement("a");
-  dropdownItem.setAttribute("class", "dropdown-item");
-  dropdownItem.setAttribute("href", "#");
-  let dropdownItem1 = dropdownItem;
-  let dropdownItem2 = dropdownItem;
-
-  logo.setAttribute("href", "index.html");
-  toggler.setAttribute("type", "button");
-  toggler.setAttribute("data-toggle", "collapse");
-  toggler.setAttribute("data-target", "#navbarSupportedContent");
-  toggler.setAttribute("aria-controls", "navbarSupportedContent");
-  toggler.setAttribute("aria-expanded", "false");
-  toggler.setAttribute("aria-label", "Toggle navigation");
-  collapseNavbar.setAttribute("class", "collapse navbar-collapse");
-  dropdownToggle.setAttribute("class", "nav-link dropdown-toggle");
-  dropdownToggle.setAttribute("href", "#");
-  dropdownToggle.setAttribute("id", "navbarDropdown");
-  dropdownToggle.setAttribute("role", "button");
-  dropdownToggle.setAttribute("data-toggle", "dropdown");
-  dropdownToggle.setAttribute("aria-haspopup", "true");
-  dropdownToggle.setAttribute("aria-expanded", "false");
-  dropdownMenu.setAttribute("href", "#")
-  dropdownMenu.setAttribute("class", "dropdown-menu dropdown-menu-right animate slideIn");
-  dropdownMenu.setAttribute("aria-labeledby", "navbarDropdown");
-  logoImg.src = Logo;
-
-  dropdownToggle.innerText = 'BurGer&Go!';
-  dropdownItem.innerText = 'Restaurants';
-  dropdownItem1.innerText = 'History';
-  dropdownItem2.innerText = 'Menu';
-
-  document.getElementById("content").appendChild(nav);
-  nav.appendChild(container);
-  container.appendChild(logo);
-  logo.appendChild(logoImg);
-  container.appendChild(toggler);
-  toggler.appendChild(togglerIcon);
-  container.appendChild(collapseNavbar);
-  collapseNavbar.appendChild(collapseUl);
-  collapseUl.appendChild(collapseLi);
-  collapseLi.appendChild(dropdownToggle);
-  collapseLi.appendChild(dropdownMenu);
-  dropdownMenu.appendChild(dropdownItem);
-  dropdownMenu.appendChild(dropdownItem1);
-  dropdownMenu.appendChild(dropdownItem2);
-}
-
-const features = () => {
-  let row = document.createElement("div");
+  let featurerow = document.createElement("div");
   let feat1 = document.createElement("div");
   let feat2 = document.createElement("div");
   let feat3 = document.createElement("div");
+  let row = document.createElement("div");
+  attributesSet();
+  render()
 
-  row.setAttribute("class", "row");
-  feat1.setAttribute("class", "col-sm-4 my-2 feature-div-1");
-  feat2.setAttribute("class", "col-sm-4 my-2 feature-div-2");
-  feat3.setAttribute("class", "col-sm-4 my-2 feature-div-3");
-
-  row.appendChild(feat1);
-  row.appendChild(feat2);
-  row.appendChild(feat3);
-
-  document.getElementById('content').appendChild(row);
+  return banner
 }
 
+const navigationBar = () => {
+
+  function addAsTabElement(tab){
+    tab.classList.add("nav-item");
+    tab.classList.add("nav-link");
+    tab.setAttribute("data-toggle", "tab");
+    tab.setAttribute("role", "tab");
+    tab.setAttribute("aria-selected", "false");
+  }
+
+  function addTabDivElement(div){
+    div.setAttribute("class", "tab-pane fade");
+    div.setAttribute("role", "tabpanel");
+  }
+
+  function attributesSet(){
+    navtabs.setAttribute("class", "nav nav-tabs nav-fill");
+    navtabs.setAttribute("id", "nav-tab");
+    navtabs.setAttribute("role", "tablist");
+    addAsTabElement(hometab);
+    hometab.classList.add("active");
+    hometab.setAttribute("id", "nav-home-tab");
+    hometab.setAttribute("href", "#nav-home");
+    hometab.setAttribute("aria-controls", "nav-home");
+    hometab.setAttribute("aria-selected", "true");
+    addAsTabElement(menutab);
+    menutab.setAttribute("id", "nav-menu-tab");
+    menutab.setAttribute("href", "#nav-menu");
+    menutab.setAttribute("aria-controls", "nav-menu");
+    addAsTabElement(contacttab);
+    contacttab.setAttribute("id", "nav-contact-tab");
+    contacttab.setAttribute("href", "#nav-contact");
+    contacttab.setAttribute("aria-controls", "nav-contact");
+    addTabDivElement(homediv);
+    homediv.classList.add("show");
+    homediv.classList.add("active");
+    homediv.setAttribute("id", "nav-home");
+    homediv.setAttribute("aria-labeledby", "nav-home-tab");
+    addTabDivElement(menudiv);
+    menudiv.setAttribute("id", "nav-menu");
+    menudiv.setAttribute("aria-labeledby", "nav-menu-tab");
+    addTabDivElement(contactdiv);
+    contactdiv.setAttribute("id", "nav-contact");
+    contactdiv.setAttribute("aria-labeledby", "nav-menu-tab");
+    tabcontent.setAttribute("id", "nav-tabContent");
+  }
+
+  function innerTextSet(){
+    menudiv.innerText = "menu";
+    contactdiv.innerText = "conatct";
+    hometab.innerText = "Home";
+    menutab.innerText = "Menu";
+    contacttab.innerText = "Contact";
+  }
+
+  function getHomeDiv() {
+    return homediv;
+  }
+
+  function render() {
+    tabcontent.appendChild(homediv);
+    tabcontent.appendChild(menudiv);
+    tabcontent.appendChild(contactdiv);
+    nav.appendChild(navtabs);
+    navtabs.appendChild(hometab);
+    navtabs.appendChild(menutab);
+    navtabs.appendChild(contacttab);
+    document.body.appendChild(nav);
+    document.body.appendChild(tabcontent);
+  }
+
+  let nav = document.createElement("nav");
+  let navtabs = document.createElement("div");
+  let hometab = document.createElement("a");
+  let menutab = document.createElement("a");
+  let contacttab = document.createElement("a");
+  let homediv = document.createElement("div");
+  let menudiv = document.createElement("div");
+  let contactdiv = document.createElement("div");
+  let tabcontent = document.createElement("div");
+
+  attributesSet();
+  innerTextSet();
+  render();
+
+  return {getHomeDiv};
+
+}
+
+let homepage = navigationBar().getHomeDiv();
 init();
-navigationBar();
-homeBanner();
+homepage.appendChild(homeTab());
 features();
