ce9eff609bae8e9cc5e107064e16bbd1a64a4393
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-05-21 16:36:12 -0500

    imported images as webṕack assets

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,20 @@
+import MenuImg0 from '../assets/Menuimg0.png';
+import MenuImg1 from '../assets/Menuimg1.png';
+import MenuImg2 from '../assets/Menuimg2.png';
+import MenuImg3 from '../assets/Menuimg3.png';
+import MenuImg4 from '../assets/Menuimg4.png';
+import MenuImg5 from '../assets/Menuimg5.png';
+import MenuImg6 from '../assets/Menuimg6.png';
+import MenuImg7 from '../assets/Menuimg7.png';
+import MenuImg8 from '../assets/Menuimg8.png';
+import MenuImg9 from '../assets/Menuimg9.png';
+import MenuImg10 from '../assets/Menuimg10.png';
+import MenuImg11 from '../assets/Menuimg11.png';
+
+const imgArray = [MenuImg0, MenuImg1, MenuImg2, MenuImg3, MenuImg4,
+MenuImg5, MenuImg6, MenuImg7, MenuImg8, MenuImg9, MenuImg10, MenuImg11];
+
+
 const menuTab = () => {
   let container = document.createElement("div");
   let header = document.createElement("h2");
@@ -32,7 +49,7 @@ const menuTab = () => {
       link.setAttribute("class", "d-block mb-4 h-100");
       let img = document.createElement("img");
       img.setAttribute("class", "img-fluid img-thumbnail");
-      img.setAttribute("src", "../src/Menuimg"+i+".png")
+      img.setAttribute("src", imgArray[i]);
       card.appendChild(link);
       link.appendChild(img);
       container.appendChild(card);
